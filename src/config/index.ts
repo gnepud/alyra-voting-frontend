a5b4ae3b934a07f5ce5c8fc392e837264e2aeb8f
gnepud <gnepud@gmail.com>
2026-07-13 14:51:01 +0200

    chore: read contract address from NEXT_PUBLIC_CONTRACT_ADDRESS environment variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -23,4 +23,4 @@ export const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/179229938']
 }
 
-export const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3' as const
+export const CONTRACT_ADDRESS = (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '0x5FbDB2315678afecb367f032d93F642f64180aa3') as `0x${string}`
